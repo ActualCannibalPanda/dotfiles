fa951d1b65dc09f1394a99ca13d71cf55db334e1
Robert O'Shea <robert.oshea@thormac.ie>
2024-12-09 13:34:56 +0000

    Changed formatting

--- lua/plugins/cmp.lua
+++ lua/plugins/cmp.lua
@@ -1,14 +1,13 @@
 return {
 	"hrsh7th/nvim-cmp",
 	dependencies = {
-		"neovim/nvim-lspconfig",
-		"hrsh7th/cmp-nvim-lsp",
-		"hrsh7th/cmp-buffer",
-		"hrsh7th/cmp-path",
-		"hrsh7th/cmp-git",
-		"hrsh7th/cmp-cmdline",
-		"hrsh7th/cmp-vsnip",
-		"hrsh7th/vim-vsnip",
+		{ "hrsh7th/cmp-nvim-lsp" },
+		{ "hrsh7th/cmp-buffer" },
+		{ "hrsh7th/cmp-path" },
+		{ "hrsh7th/cmp-git" },
+		{ "hrsh7th/cmp-cmdline" },
+		{ "hrsh7th/cmp-vsnip" },
+		{ "hrsh7th/vim-vsnip" },
 	},
 	config = function()
 		-- Set up nvim-cmp.
