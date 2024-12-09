169b87acbf1d114dc1ddabf420308a26871ee06e
Robert O'Shea <robert.oshea@thormac.ie>
2024-12-09 13:21:06 +0000

    Update formatting

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,21 +1,21 @@
 return {
-	"nvim-treesitter/nvim-treesitter",
-	build = ":TSUpdate",
-	config = function()
-		require("nvim-treesitter.configs").setup({
-			ensure_installed = { "lua", "rust", "c", "cpp", "toml" },
-			auto_install = true,
-			sync_install = false,
-			highlight = {
-				enable = true,
-				additional_vim_regex_highlighting = false,
-			},
-			indent = { enable = true },
-			rainbow = {
-				enable = true,
-				extended_mode = true,
-				max_file_lines = nil,
-			},
-		})
-	end,
+  "nvim-treesitter/nvim-treesitter",
+  build = ":TSUpdate",
+  config = function()
+    require("nvim-treesitter.configs").setup({
+      ensure_installed = { "lua", "rust", "c", "cpp", "toml" },
+      auto_install = true,
+      sync_install = false,
+      highlight = {
+        enable = true,
+        additional_vim_regex_highlighting = false,
+      },
+      indent = { enable = true },
+      rainbow = {
+        enable = true,
+        extended_mode = true,
+        max_file_lines = nil,
+      },
+    })
+  end,
 }
