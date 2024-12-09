7c7a16677d44d541bf5dcda65f0759e21914fcad
Robert O'Shea <robert.oshea@thormac.ie>
2024-12-09 13:58:51 +0000

    Add rainbow brackets

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,21 +1,22 @@
 return {
-  "nvim-treesitter/nvim-treesitter",
-  build = ":TSUpdate",
-  config = function()
-    require("nvim-treesitter.configs").setup({
-      ensure_installed = { "lua", "rust", "c", "cpp", "toml" },
-      auto_install = true,
-      sync_install = false,
-      highlight = {
-        enable = true,
-        additional_vim_regex_highlighting = false,
-      },
-      indent = { enable = true },
-      rainbow = {
-        enable = true,
-        extended_mode = true,
-        max_file_lines = nil,
-      },
-    })
-  end,
+	"nvim-treesitter/nvim-treesitter",
+	dependencies = { "HiPhish/nvim-ts-rainbow2" },
+	build = ":TSUpdate",
+	config = function()
+		require("nvim-treesitter.configs").setup({
+			ensure_installed = { "lua", "rust", "c", "cpp", "toml" },
+			auto_install = true,
+			sync_install = false,
+			highlight = {
+				enable = true,
+				additional_vim_regex_highlighting = false,
+			},
+			indent = { enable = true },
+			rainbow = {
+				enable = true,
+				query = "rainbow-parens",
+				strategy = require("ts-rainbow").strategy.global,
+			},
+		})
+	end,
 }
