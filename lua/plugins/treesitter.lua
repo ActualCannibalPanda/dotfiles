c115686f352505bd748388ac5d51bb5303c0624b
Robert O'Shea <robert.oshea@thormac.ie>
2024-12-09 12:24:41 +0000

    Added Python

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,14 +1,16 @@
 return {
 	"nvim-treesitter/nvim-treesitter",
+	build = ":TSUpdate",
 	config = function()
 		require("nvim-treesitter.configs").setup({
-			ensure_installed = { "lua", "rust", "toml" },
+			ensure_installed = { "lua", "rust", "c", "cpp", "toml" },
 			auto_install = true,
+			sync_install = false,
 			highlight = {
 				enable = true,
 				additional_vim_regex_highlighting = false,
 			},
-			ident = { enable = true },
+			indent = { enable = true },
 			rainbow = {
 				enable = true,
 				extended_mode = true,
