3eb01d685c6a09badd06242265284e67a1a2f1e9
Robert O'Shea <robert.oshea@thormac.ie>
2024-12-10 11:44:44 +0000

    Added kemyap for toggleterm

--- lua/plugins/toggleterm.lua
+++ lua/plugins/toggleterm.lua
@@ -1,21 +1,43 @@
 return {
-  "akinsho/toggleterm.nvim",
-  version = "*",
-  opts = {},
-  config = function()
-    require("toggleterm").setup()
-    local Terminal = require("toggleterm.terminal").Terminal
-    local lazygit = Terminal:new({
-      cmd = "lazygit",
-      hidden = true,
-      direction = "float",
-    })
+	"akinsho/toggleterm.nvim",
+	version = "*",
+	opts = {},
+	config = function()
+		require("toggleterm").setup()
+		local Terminal = require("toggleterm.terminal").Terminal
+		local lazygit = Terminal:new({
+			cmd = "lazygit",
+			hidden = true,
+			direction = "float",
+		})
 
-    function _lazygit_toggle()
-      lazygit:toggle()
-    end
+		function _lazygit_toggle()
+			lazygit:toggle()
+		end
 
-    vim.api.nvim_set_keymap("n", "<leader>gg", "<cmd>lua _lazygit_toggle()<cr>",
-      { desc = "Open LazyGit", noremap = true, silent = true })
-  end,
+		vim.keymap.set(
+			"n",
+			"<leader>gg",
+			"<cmd>lua _lazygit_toggle()<cr>",
+			{ desc = "Open LazyGit", noremap = true, silent = true }
+		)
+		vim.keymap.set("n", "<leader>tt", function()
+			vim.cmd([[ ToggleTermToggleAll ]])
+
+			local buffers = vim.api.nvim_list_bufs()
+
+			local toggleterm_exists = false
+			for _, buf in ipairs(buffers) do
+				local buf_name = vim.api.nvim_buf_get_name(buf)
+				if buf_name:find("toggleterm#") then
+					toggleterm_exists = true
+					break
+				end
+			end
+
+			if not toggleterm_exists then
+				vim.cmd([[ exe 1 . "ToggleTerm direction=float" ]])
+			end
+		end, { desc = "Open terminal", noremap = true })
+	end,
 }
