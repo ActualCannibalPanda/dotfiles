3c85dbcf824043c66e07f826a066b820772aef57
Robert O'Shea <robertosheagamedev@gmail.com>
2024-12-15 21:33:51 +0000

    Add odin run command

--- lua/robertos/plugins/lsp/odin.lua
+++ lua/robertos/plugins/lsp/odin.lua
@@ -3,5 +3,11 @@ return {
     require('lspconfig').ols.setup({
       capabilities = capabilities,
     })
+    vim.keymap.set(
+      'n',
+      '<localleader>rr',
+      '<cmd>:TermExec cmd="odin run ." direction=float<cr>',
+      { desc = 'Run odin project' }
+    )
   end,
 }
