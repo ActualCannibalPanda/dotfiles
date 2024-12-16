5f5ef80020e8fec4ceeb53104bd8c0cfd2fe452a
Robert O'Shea <robertosheagamedev@gmail.com>
2024-12-16 00:25:11 +0000

    Add wezterm config

---
+++ wezterm/wezterm.lua
@@ -0,0 +1,25 @@
+local wezterm = require('wezterm')
+
+local default_prog = {}
+local keys = {
+  { key = 'l', mods = 'ALT', action = wezterm.action.ShowLauncher },
+}
+
+local launch_menu = {}
+if wezterm.target_triple == 'x86_64-pc-windows-msvc' then
+  default_prog = {
+    'powershell.exe',
+    '-noe',
+    '-c',
+    '&{Import-Module "C:/Program Files/Microsoft Visual Studio/2022/Community/Common7/Tools/Microsoft.VisualStudio.DevShell.dll"; Enter-VsDevShell  63abf6e5}',
+  }
+else
+  default_prog = { '/bin/bash' }
+end
+
+return {
+  keys = keys,
+  default_prog = default_prog,
+  launch_menu = launch_menu,
+  color_scheme = 'MaterialOcean',
+}
