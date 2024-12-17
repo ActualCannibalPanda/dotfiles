5073db7eb19d6430c5109ca185b4cba8c5fe6102
Robert O'Shea <robert.oshea@thormac.ie>
2024-12-17 13:51:41 +0000

    Update wezterm.lua

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -7,11 +7,17 @@ local keys = {
 
 local launch_menu = {}
 if wezterm.target_triple == 'x86_64-pc-windows-msvc' then
-  default_prog = {
-    'powershell.exe',
-    '-noe',
-    '-c',
-    '&{Import-Module "C:/Program Files/Microsoft Visual Studio/2022/Community/Common7/Tools/Microsoft.VisualStudio.DevShell.dll"; Enter-VsDevShell  63abf6e5}',
+  default_prog = { 'powershell.exe' }
+  launch_menu = {
+    {
+      label = 'Micrsoft Visual Sutdio Developer Console',
+      args = {
+        'powershell.exe',
+        '-noe',
+        '-c',
+        '&{Import-Module "C:/Program Files/Microsoft Visual Studio/2022/Community/Common7/Tools/Microsoft.VisualStudio.DevShell.dll"; Enter-VsDevShell  63abf6e5}',
+      },
+    },
   }
 else
   default_prog = { '/bin/bash' }
