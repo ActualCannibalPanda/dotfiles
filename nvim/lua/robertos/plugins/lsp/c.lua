51afcfbbebb2044c2dca70eb0d71ebcf690425e0
Robert O'Shea <robertosheagamedev@gmail.com>
2025-01-13 00:26:48 +0000

    Added support for meson root dirs

--- nvim/lua/robertos/plugins/lsp/c.lua
+++ nvim/lua/robertos/plugins/lsp/c.lua
@@ -1,6 +1,11 @@
 local on_attach = require('robertos.plugins.lsp.on_attach')
 return {
   setup = function(capabilities)
-    require('lspconfig').clangd.setup({ capabilities = capabilities, on_attach = on_attach })
+    require('lspconfig').clangd.setup({
+      root_dir = require('lspconfig').util.root_pattern('meson.build', 'CMakeLists.txt', 'Makefile'),
+      capabilities = capabilities,
+      on_attach = on_attach
+    })
   end,
+
 }
