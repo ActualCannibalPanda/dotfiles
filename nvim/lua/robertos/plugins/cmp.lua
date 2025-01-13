98d46b86f2a273f7f53c456da06c0d591eda1f6f
Robert O'Shea <robertosheagamedev@gmail.com>
2025-01-13 00:26:56 +0000

    Formatting

--- nvim/lua/robertos/plugins/cmp.lua
+++ nvim/lua/robertos/plugins/cmp.lua
@@ -38,11 +38,11 @@ return {
       }),
       sources = cmp.config.sources({
         { name = 'path' },
-        { name = 'nvim_lsp', keyword_length = 3 },
+        { name = 'nvim_lsp',               keyword_length = 3 },
         { namd = 'nvim_lsp_signature_help' },
-        { name = 'nvim_lua', keyword_length = 2 },
-        { name = 'buffer', keyword_length = 2 },
-        { name = 'vsnip', keyword_length = 2 }, -- For vsnip users.
+        { name = 'nvim_lua',               keyword_length = 2 },
+        { name = 'buffer',                 keyword_length = 2 },
+        { name = 'vsnip',                  keyword_length = 2 }, -- For vsnip users.
         { name = 'calc' },
       }),
       formatting = {
