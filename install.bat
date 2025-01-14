bf64b575b31c7319e83687593ccd336ca44f4178
Robert O'Shea <robertosheagamedev@gmail.com>
2025-01-14 18:18:45 +0000

    Add zig support

--- install.bat
+++ install.bat
@@ -1,5 +1,11 @@
 @echo off
 
+if not exist %LOCALAPPDATA%\nvim (
 mklink /J  %LOCALAPPDATA%\nvim nvim
+)
+
+if not exist %LOCALAPPDATA%\zls.json (
+  mklink %LOCALAPPDATA%\zls.json zls.json
+)
 
 @echo on
