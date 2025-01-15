18c0559d00b26683b28d73d95bb377ce013447e1
Robert O'Shea <robert.oshea@thormac.ie>
2025-01-15 10:27:35 +0000

    Add which-key

---
+++ nvim/lua/robertos/plugins/which-key.lua
@@ -0,0 +1,19 @@
+return {
+  'folke/which-key.nvim',
+  event = 'VeryLazy',
+  config = function()
+    local wk = require('which-key')
+    wk.add({
+      { '<leader>h', group = 'Harpoon' },
+    })
+  end,
+  keys = {
+    {
+      '<leader>?',
+      function()
+        require('which-key').show({ global = false })
+      end,
+      desc = 'Buffer Local Keymaps (which-key)',
+    },
+  },
+}
