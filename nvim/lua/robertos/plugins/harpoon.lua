e0d46f6d7cc24a86145d40930175cd82a2e6d961
Robert O'Shea <robert.oshea@thormac.ie>
2025-01-15 11:16:42 +0000

    Adjusted which-key

--- nvim/lua/robertos/plugins/harpoon.lua
+++ nvim/lua/robertos/plugins/harpoon.lua
@@ -1,6 +1,7 @@
 return {
   'ThePrimeagen/harpoon',
   dependencies = { 'nvim-lua/plenary.nvim' },
+  opts = {},
   keys = {
     {
       '<leader>hf',
--- nvim/lua/robertos/plugins/which-key.lua
+++ nvim/lua/robertos/plugins/which-key.lua
@@ -1,10 +1,13 @@
 return {
   'folke/which-key.nvim',
-  event = 'VeryLazy',
   config = function()
     local wk = require('which-key')
     wk.add({
-      { '<leader>h', group = 'Harpoon' },
+      mode = { 'n', 'v' },
+      {
+        '<leader>h',
+        group = 'Harpoon',
+      },
     })
   end,
   keys = {
