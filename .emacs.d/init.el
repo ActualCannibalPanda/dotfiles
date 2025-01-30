6807ce81579f47df110efcb43c94c49d2c1b6ab9
Robert O'Shea <robert.oshea@thormac.ie>
2025-01-30 12:56:46 +0000

    Add emacs config

---
+++ .emacs.d/init.el
@@ -0,0 +1,27 @@
+(require 'package)
+(package-initialize)
+(setq package-enable-at-startup nil)
+
+(setq package-archives
+      '(("ELPA"  . "http://tromey.com/elpa/")
+	("gnu"   . "http://elpa.gnu.org/packages/")
+	("melpa" . "https://melpa.org/packages/")
+	("org"   . "https://orgmode.org/elpa/")))
+
+(setq package-selected-packages
+      (quote ( ; theme
+	cyberpunk-theme)))
+		     
+(unless (package-installed-p 'use-package)
+  (package-refresh-contents)
+  (package-install 'use-package))
+
+(eval-when-compile (require 'use-package))
+
+(use-package use-package
+  :config
+  (setq use-package-always-ensure t))
+
+(add-hook 'after-init-hook
+	  (lambda ()
+	    (load-theme 'cyberpunk t)))
--- install.bat
+++ install.bat
@@ -4,6 +4,10 @@ if not exist %LOCALAPPDATA%\nvim (
 mklink /J  %LOCALAPPDATA%\nvim nvim
 )
 
+if not exist %USERPROFILE%\.emacs.d (
+mklink /J  %USERPROFILE%\.emacs.d .emacs.d
+)
+
 if exist %LOCALAPPDATA%\zls.json (
   del %LOCALAPPDATA%\zls.json
 )
