40bc68d8fbd62f2267c7c7c06822db21d485b91c
Robert O'Shea <robert.oshea@thormac.ie>
2025-01-31 13:54:44 +0000

    Add eglot

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -62,7 +62,7 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
-   '(org-contrib org catppuccin-theme company magit rust-mode cargo)))
+   '(eglot org-contrib org catppuccin-theme company magit rust-mode cargo)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
