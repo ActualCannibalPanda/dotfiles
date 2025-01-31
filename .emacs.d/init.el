fb87a819f5291d7ab9534e276f9592ec8768fa48
Robert O'Shea <robert.oshea@thormac.ie>
2025-01-31 13:37:09 +0000

    Add non-gnu

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -8,10 +8,11 @@
 (setq package-enable-at-startup nil)
 
 (setq package-archives
-      '(("ELPA"  . "http://tromey.com/elpa/")
-	("gnu"   . "http://elpa.gnu.org/packages/")
-	("melpa" . "https://melpa.org/packages/")
-	("org"   . "https://orgmode.org/elpa/")))
+      '(("ELPA"   . "http://tromey.com/elpa/")
+	("gnu"    . "http://elpa.gnu.org/packages/")
+	("melpa"  . "https://melpa.org/packages/")
+	("org"    . "https://orgmode.org/elpa/")
+	("nongnu" . "https://elpa.nongnu.org/nongnu/")))
 
 (package-initialize)
 
@@ -55,3 +56,16 @@
 
 (load "keymaps")
 (load "langs")
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   '(org-contrib org catppuccin-theme company magit rust-mode cargo)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
