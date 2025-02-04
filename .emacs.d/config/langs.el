28e71d79b5c13053375576ab01ff44a376bdae21
Robert O'Shea <robert.oshea@thormac.ie>
2025-02-04 10:13:53 +0000

    Add python

--- .emacs.d/config/langs.el
+++ .emacs.d/config/langs.el
@@ -1,8 +1,12 @@
 (load "c")
 (load "rust")
+(load "python")
 
 (use-package lsp-mode
   :init
   (setq lsp-keymap-prefix "C-c l")
   :hook ((rust-mode . lsp-deferred))
   :commands lsp)
+
+(use-package lsp-ui
+  :commands lsp-ui-mode)
