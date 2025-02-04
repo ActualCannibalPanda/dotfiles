fee6e0cac706e697e1d0ca8feb4d60ef766d6364
Robert O'Shea <robert.oshea@thormac.ie>
2025-02-04 13:41:57 +0000

    Add Quelpa

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -50,6 +50,17 @@
   (unless (package-installed-p package)
     (package-install package)))
 
+(unless (package-installed-p 'quelpa)
+  (with-temp-buffer
+    (url-insert-file-contents "https://raw.githubusercontent.com/quelpa/quelpa/master/quelpa.el")
+    (eval-buffer)
+    (quelpa-self-upgrade)))
+
+(quelpa
+ '(quelpa-use-package
+   :fetcher git
+   :url "https://github.com/quelpa/quelpa-use-package.git"))
+(require 'quelpa-use-package)
 (eval-when-compile (require 'use-package))
 
 (use-package use-package
