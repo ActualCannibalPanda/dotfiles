a72cea1996027d2dd8bfed230d3180ff4f7d0aac
Robert O'Shea <robertosheagamedev@gmail.com>
2025-03-18 11:59:07 +0000

    Add Hideshow

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -494,6 +494,11 @@
   :bind (:map dired-mode-map
 	      ("-" . dired-up-directory)))
 
+(use-package hideshow
+  :ensure nil
+  :bind (("C-<tab>" . hs-toggle-hiding))
+  :hook (prog-mode . hs-minor-mode))
+
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
