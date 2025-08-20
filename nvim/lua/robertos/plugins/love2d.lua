b9735508b50387931e606088f7763b9370ad926e
Robert O'Shea <robertosheagamedev@gmail.com>
2025-08-21 00:59:57 +0100

    Add Love2D support

---
+++ nvim/lua/robertos/plugins/love2d.lua
@@ -0,0 +1,11 @@
+return {
+  "S1M0N38/love2d.nvim",
+  event = "VeryLazy",
+  version = "2.*",
+  opts = {},
+  keys = {
+    { "<leader>v",  ft = "lua",          desc = "Love2D" },
+    { "<leader>vv", "<cmd>LoveRun<cr>",  ft = "lua",     desc = "Run Love2D" },
+    { "<leader>vs", "<cmd>LoveStop<cr>", ft = "lua",     desc = "Stop Love2D" }
+  },
+}
