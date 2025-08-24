9c1753a9f08826130eeefc9688e23d2609ffee1e
Robert O'Shea <robertosheagamedev@gmail.com>
2025-08-24 04:06:54 +0100

    Configure orgmode

--- nvim/lua/robertos/plugins/orgmode.lua
+++ nvim/lua/robertos/plugins/orgmode.lua
@@ -3,7 +3,24 @@ return {
   event = 'VeryLazy',
   config = function()
     require('orgmode').setup({
-      org_agenda_files = '~/orgfiles/**/*',
+      org_agenda_files = '~/org/**/*',
+      org_default_notes_file = '~/org/notes.org',
+      org_capture_templates = {
+        h = {
+          description = "Hack 'n' Slash Todo",
+          template = '* TODO %?\n',
+          target = '~/org/hacknslash.org',
+          heading = 'Todos',
+        },
+        s = {
+          description = 'Shooter Todo',
+          template = '* TODO %?\n',
+          target = '~/org/shooter.org',
+          heading = 'Todos',
+        },
+      },
+      org_log_done = false,
+      org_agenda_skip_scheduled_if_done = false,
     })
   end,
 }
