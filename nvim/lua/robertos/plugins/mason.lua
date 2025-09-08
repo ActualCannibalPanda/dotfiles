5205387b6a8d4b366c765498f536becb1873a70e
Robert O'Shea <robertosheagamedev@gmail.com>
2025-09-08 06:44:54 +0100

    Removed mason-lspconfig

--- nvim/lua/robertos/plugins/mason.lua
+++ nvim/lua/robertos/plugins/mason.lua
@@ -1,7 +1,6 @@
 return {
   {
     'williamboman/mason.nvim',
-    dependencies = { { 'williamboman/mason-lspconfig.nvim', module = 'mason' } },
     config = function()
       require('mason').setup({
         ui = {
@@ -11,17 +10,6 @@ return {
             package_uninstalled = '',
           },
         },
-      })
-      require('mason-lspconfig').setup({
-        ensure_installed = {
-          'clangd',
-          'haxe_language_server',
-          'lua_ls',
-          'pylyzer',
-          'ols',
-          'rust_analyzer',
-        },
-        automatic_install = true,
       })
     end,
   },
