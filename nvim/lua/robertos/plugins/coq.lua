02e9bb4f0271dd4e8bee9da77901171b7b5bee5c
Robert O'Shea <robertosheagamedev@gmail.com>
2025-09-11 00:49:37 +0100

    Add keybind for renaming

--- nvim/lua/robertos/plugins/coq.lua
+++ nvim/lua/robertos/plugins/coq.lua
@@ -13,5 +13,7 @@ return {
     local coq = require('coq')
     vim.lsp.config('lua_ls', coq.lsp_ensure_capabilities())
     vim.lsp.enable('lua_ls')
+
+    vim.keymap.set('n', '<F2>', vim.lsp.buf.rename)
   end,
 }
