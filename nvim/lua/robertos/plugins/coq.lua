7515b60a4ed55f1d920181ad43e4a85992904547
Robert O'Shea <robertosheagamedev@gmail.com>
2025-09-10 04:05:58 +0100

    Replace cmp with coq

---
+++ nvim/lua/robertos/plugins/coq.lua
@@ -0,0 +1,17 @@
+return {
+  'neovim/nvim-lspconfig',
+  dependencies = {
+    { 'ms-jpq/coq_nvim', branch = 'coq' },
+    { 'ms-jpq/coq.artifacts', branch = 'artifacts' },
+  },
+  init = function()
+    vim.g.coq_settings = {
+      auto_start = true,
+    }
+  end,
+  config = function()
+    local coq = require('coq')
+    vim.lsp.config('lua_ls', coq.lsp_ensure_capabilities())
+    vim.lsp.enable('lua_ls')
+  end,
+}
