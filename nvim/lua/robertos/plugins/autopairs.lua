ac7f3933468451ffe985daf804a33d09c5aa3b9e
Robert O'Shea <robertosheagamedev@gmail.com>
2025-09-10 04:06:15 +0100

    Fix issue with autopairs

--- nvim/lua/robertos/plugins/autopairs.lua
+++ nvim/lua/robertos/plugins/autopairs.lua
@@ -1,18 +1,14 @@
 return {
   'windwp/nvim-autopairs',
   event = 'InsertEnter',
-  config = function()
-    reuquire('cmp')
-    local cmp_autopairs = require('nvim-autopairs.completion.cmp')
-
-    cmp.event:on('confirm_done', cmp_autopairs.on_cofirm_done())
-  end,
-  opts = {
-    check_ts = true,
-    ts_config = {
-      lua = {
-        'string',
+  config = function(_, opts)
+    require('nvim-autopairs').setup({
+      check_ts = true,
+      ts_config = {
+        lua = {
+          'string',
+        },
       },
-    },
-  },
+    })
+  end,
 }
