d5b6e89a22e7d4043060cb66955998ff6b63c759
Robert O'Shea <robertosheagamedev@gmail.com>
2025-10-21 15:21:14 +0100

    Add cargo to PATH

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -8,4 +8,8 @@ config.color_scheme = 'AdventureTime'
 config.window_background_opacity = 0.88
 config.use_fancy_tab_bar = false
 
+config.set_environment_variables = {
+  PATH =  wezterm.home_dir .. "/.cargo/bin/:" .. os.getenv "PATH",
+}
+
 return config
