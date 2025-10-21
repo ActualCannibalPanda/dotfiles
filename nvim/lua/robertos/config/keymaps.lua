b5da66d79105f00a547144896a0c0b0b1a3aaa38
Robert O'Shea <robertosheagamedev@gmail.com>
2025-10-21 15:20:29 +0100

    Add quick escape from insert mode

--- nvim/lua/robertos/config/keymaps.lua
+++ nvim/lua/robertos/config/keymaps.lua
@@ -2,3 +2,4 @@ local s = vim.keymap.set
 s('n', '<leader>lz', '<cmd>Lazy<cr>', { desc = 'Open Lazy UI' })
 s('n', '<leader>;', '<cmd>nohlsearch<cr>', { desc = 'Stop Highlighting search' })
 s('n', '<C-t>c', '<cmd>tabc<cr>', { desc = 'Close tab' })
+s('i', 'jk', '<ESC>', { desc = 'Quick escape insert mode' })
