47a18d20ed8286b8a15b17bf842b927f295725b3
Robert O'Shea <robertosheagamedev@gmail.com>
2025-12-30 20:56:52 +0000

    Fix issues with treesitter

--- nvim/lua/robertos/plugins/treesitter.lua
+++ nvim/lua/robertos/plugins/treesitter.lua
@@ -1,31 +1,37 @@
 return {
   'nvim-treesitter/nvim-treesitter',
+  lazy = false,
   branch = 'main',
   dependencies = { 'HiPhish/rainbow-delimiters.nvim' },
   build = ':TSUpdate',
   config = function()
-    require('nvim-treesitter.configs').setup({
-      ensure_installed = {
-        'lua',
-        'rust',
-        'c',
-        'cpp',
-        'toml',
-        'zig',
-        'odin',
-        'gdscript',
-      },
-      auto_install = true,
-      highlight = {
-        enable = true,
-        additional_vim_regex_highlighting = false,
-      },
-      indent = { enable = true },
-    })
+    local ts = require('nvim-treesitter')
+    ts.setup()
+    local fts = {
+      'lua',
+      'rust',
+      'c',
+      'cpp',
+      'toml',
+      'zig',
+      'odin',
+      'gdscript',
+    }
 
-    if vim.loop.os_uname().sysname == 'Windows_NT' then
-      require('nvim-treesitter.install').compilers = { 'zig' }
+    ts.install(fts)
+
+    for _, ft in pairs(fts) do
+      vim.api.nvim_create_autocmd('FileType', {
+        pattern = { ft },
+        callback = function()
+          vim.treesitter.start()
+          vim.wo[0][0].foldexpr = 'v:lua.vim.treesitter.foldexpr()'
+          vim.wo[0][0].foldmethod = 'expr'
+          vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
+        end,
+      })
     end
+
     require('rainbow-delimiters.setup').setup({})
   end,
 }
