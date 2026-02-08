009dac940bc5c50c230e80cab8ff071f6d3027ba
Robert O'Shea <robertosheagamedev@gmail.com>
2026-02-08 01:18:16 +0000

    Stopped tap completing completions

--- nvim/lua/robertos/plugins/cmp.lua
+++ nvim/lua/robertos/plugins/cmp.lua
@@ -40,10 +40,9 @@ return {
       mapping = cmp.mapping.preset.insert({
         ['<C-b>'] = cmp.mapping.scroll_docs(-4),
         ['<C-f>'] = cmp.mapping.scroll_docs(4),
-        ['<C-Space>'] = cmp.mapping.complete(),
         ['<C-e>'] = cmp.mapping.abort(),
         ['<CR>'] = cmp.mapping.confirm({ select = true }),
-        ['<Tab>'] = cmp.mapping(function(fallback)
+        ['<C-space>'] = cmp.mapping(function(fallback)
           if cmp.visible() then
             cmp.select_next_item()
           else
